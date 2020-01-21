4546a534a73bd2cf44609ff5fb7373e877ba6346
qhalsey <quentin.developer@outlook.com>
2020-01-21 14:37:19 -0800

    Finished day One

--- Answers/one.py
+++ Answers/one.py
@@ -18,3 +18,49 @@ def special_numbers():
     print(print_string)
 
 special_numbers()
+
+# Question 2
+
+### **Question:**
+
+##Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8
+##Then, the output should be:40320
+
+##--------------------
+### Hints:
+##In case of input data being supplied to the question, it should be assumed to be a console input.***
+
+def factorial(input):
+	factorial_answer = 1
+	for i in range(1, input + 1):
+		factorial_answer = factorial_answer * i
+
+	print(factorial_answer)
+
+factorial(8)
+
+# Question 3
+
+### **Question:**
+
+##With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8***
+
+##Then, the output should be:***
+
+##{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+##------------------
+
+### Hints: 
+##In case of input data being supplied to the question, it should be assumed to be a console input.Consider use dict()***
+
+##-----------------
+
+def dictionary_squared(input):
+	dictionary = {}
+	for i in range(1, input + 1):
+		dictionary[i] = i * i
+
+	print(dictionary)
+
+dictionary_squared(8)
